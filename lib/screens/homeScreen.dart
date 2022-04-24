67edfbfc7a64ad82b7d36566d00c19278f644a04
Abdulaziz Umar Jada (Adzumr) <adzumrjada@gmail.com>
2022-04-24 12:29:51 +0100

    added shimmer effect

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -7,6 +7,8 @@ import 'package:movie_app/models/constants.dart';
 import 'package:movie_app/models/movieModel.dart';
 import 'package:tmdb_api/tmdb_api.dart';
 
+import '../models/skeleton.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -15,8 +17,15 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late bool _isLoading;
   @override
   void initState() {
+    _isLoading = true;
+    Future.delayed(const Duration(seconds: 3), () {
+      setState(() {
+        _isLoading = false;
+      });
+    });
     loadMovies();
     super.initState();
   }
@@ -24,7 +33,6 @@ class _HomeScreenState extends State<HomeScreen> {
   Constants constants = Constants();
   List topRAtedList = [];
   List trendingTVList = [];
-  // TmdbModel tmdbModel = TmdbModel.fromJson();
 
   loadMovies() async {
     TMDB tmdbWithLogs = TMDB(ApiKeys(constants.apiKey, constants.accessToken),
@@ -47,25 +55,72 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(20),
+        child: _isLoading
+            ? ListView.separated(
+                itemCount: 5,
+                itemBuilder: (context, index) => const NewsCardSkelton(),
+                separatorBuilder: (context, index) =>
+                    const SizedBox(height: 16),
+              )
+            : Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  children: [
+                    const Text("Trending Movies"),
+                    Expanded(
+                      child: MovieDetail(
+                        movieInfo: topRAtedList,
+                      ),
+                    ),
+                    Expanded(
+                      child: MovieDetail(
+                        movieInfo: trendingTVList,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+      ),
+    );
+  }
+}
+
+class NewsCardSkelton extends StatelessWidget {
+  const NewsCardSkelton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        const Skeleton(height: 120, width: 120),
+        const SizedBox(width: 16),
+        Expanded(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const Text("Trending Movies"),
-              Expanded(
-                child: MovieDetail(
-                  movieInfo: topRAtedList,
-                ),
-              ),
-              Expanded(
-                child: MovieDetail(
-                  movieInfo: trendingTVList,
-                ),
-              ),
+              const Skeleton(width: 80),
+              const SizedBox(height: 16 / 2),
+              const Skeleton(),
+              const SizedBox(height: 16 / 2),
+              const Skeleton(),
+              const SizedBox(height: 16 / 2),
+              Row(
+                children: const [
+                  Expanded(
+                    child: Skeleton(),
+                  ),
+                  SizedBox(width: 16),
+                  Expanded(
+                    child: Skeleton(),
+                  ),
+                ],
+              )
             ],
           ),
-        ),
-      ),
+        )
+      ],
     );
   }
 }
